1788abfbf9bb511a5620f032d8bbc6065a6c19d9
NaotoSakamoto <nao02to04@icloud.com>
2022-09-08 09:48:04 +0000

    Split submission form ver1

--- resources/views/learncodes/form.blade.php
+++ resources/views/learncodes/form.blade.php
@@ -1,6 +1,8 @@
 {!! Form::open(['route' => 'learncodes.store']) !!}
     <div class="form-group">
-        {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
+        タイトル{!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '1']) !!}
+        言語{!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '1']) !!}
+        学習時間{!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
         {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
     </div>
 {!! Form::close() !!}
